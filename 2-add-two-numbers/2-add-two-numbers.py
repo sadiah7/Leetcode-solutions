6a8290ac1054491331252d5fb8b3ebfbf02f1fb2
Sadiah Shaikh <69952394+sadiah7@users.noreply.github.com>
2022-06-28 13:25:51 +0530

    Time: 83 ms (72.88%), Space: 14 MB (43.27%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -9,35 +9,23 @@ class Solution:
         carry = 0
         while l1 and l2:
             total = l1.val + l2.val + carry
-            if total > 9:
-                cur.next = ListNode(total - 10)
-                carry = 1
-            else:
-                cur.next = ListNode(total)
-                carry = 0
+            cur.next = ListNode(total % 10)
+            carry = (l1.val + l2.val + carry) // 10
             cur = cur.next
             l1 = l1.next
             l2 = l2.next
         
         while l1:
             total = l1.val + carry
-            if total > 9:
-                cur.next = ListNode(total - 10)
-                carry = 1
-            else:
-                cur.next = ListNode(total)
-                carry = 0
+            cur.next = ListNode(total % 10)
+            carry = (l1.val + carry) // 10
             cur = cur.next
             l1 = l1.next
         
         while l2:
             total = l2.val + carry
-            if total > 9:
-                cur.next = ListNode(total - 10)
-                carry = 1
-            else:
-                cur.next = ListNode(total)
-                carry = 0
+            cur.next = ListNode(total % 10)
+            carry = ( l2.val + carry) // 10
             cur = cur.next
             l2 = l2.next
         
